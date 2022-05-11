8c0243c7c4b58e1a8d374bc0956a2972b29484ce
Danvers Morgan <morgan.danvers@hotmail.fr>
2022-05-11 23:21:33 +0200

    correction des images effectuées et correction de la route like ainsi que le controller sauce.js en cours

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,10 +1,10 @@
 const multer = require("multer");
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
+// const MIME_TYPES = {
+//   "image/jpg": "jpg",
+//   "image/jpeg": "jpg",
+//   "image/png": "png",
+// };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -12,8 +12,8 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    // const extension = MIME_TYPES[file.mimetype];
+    callback(null,Date.now() + '_' + name);
   },
 });
 
