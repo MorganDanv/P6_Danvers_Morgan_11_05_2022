c47eee76169dadfae578f0f0dd88dcd38f525241
Danvers Morgan <morgan.danvers@hotmail.fr>
2022-05-27 10:54:55 +0200

    modification de sécurité sur multer effectué, projet terminé

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,17 @@
 const multer = require("multer");
 
-// const MIME_TYPES = {
-//   "image/jpg": "jpg",
-//   "image/jpeg": "jpg",
-//   "image/png": "png",
-// };
-
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "images");
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
-    // const extension = MIME_TYPES[file.mimetype];
-    callback(null,Date.now() + '_' + name);
-  },
+    var ext = path.extname(file.originalname);
+    if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+        return callback(new Error('Only images are allowed'))
+    }
+      callback(null,Date.now() + '_' + name);
+  }, 
 });
 
 module.exports = multer({ storage: storage }).single("image");
